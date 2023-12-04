157589e3f61ddeb93ee1c18733e529d5d2808994
Sharayu <sharayushendre7@gmail.com>
2023-12-04 16:15:53 +0530

    Configured

--- pages/MyProfile/[userAddress]/index.tsx
+++ pages/MyProfile/[userAddress]/index.tsx
@@ -1,6 +1,7 @@
 import { NFTContract } from '@/Blockend/interact';
 import web3 from '@/Blockend/web3';
-import { Box } from '@chakra-ui/react';
+import Navbar from '@/Component/Navbar/Navbar';
+import { Box, Center, Heading, Link, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
 
@@ -25,18 +26,27 @@ useEffect(() => {
 },[NFTContract])
 
   return (<>
+
     <Box  width={"100%"} height={"100vh"} style={{
-          backgroundImage: `url(https://images.unsplash.com/photo-1452421822248-d4c2b47f0c81?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
+          backgroundImage: `url(https://images.unsplash.com/photo-1483683804023-6ccdb62f86ef?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundPosition: "center center"
         }}>
-  {nftList && nftList?.map((nft:any) => (
-    <div key={nft?.tokenID}>
-      <p>{nft?.tokenID}</p>
-      <p>{nft?.tokenURI}</p>
-    </div>
-  ))}
+            <Navbar/>
+    <Center>
+  <Heading mt={10}textShadow={  "rgb(7, 7, 7) 1px 0 10px"} color={"white"}>My NFTs</Heading>     
+  </Center>
+<Box mx={20}mt={10} width={"200px"}bgColor={"whiteAlpha.800"} p={10} borderRadius={10} color={"black"}>
+  <Text fontSize={20}>Token ID</Text>
+       {nftList?.[0]?.[0]}
+  <Text fontSize={20}>Token URI</Text>
+  <Link href = {nftList?.[1]?.[0]} color={"blue"} textDecor={"underline"}>
+    JW Marriott Exclusive NFT
+  </Link>
+</Box>
+
+
   </Box>
   </>
   )
